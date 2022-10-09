3e68e04a3e2b1eda9ccccc3f95c512588e34b20d
sbahala <sbahala@stevens.edu>
2022-10-10 01:32:04 +0530

    Testcases

---
+++ TestGitHubApi.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Test class to test GitHub Api call
+@author: Sushmita bahala
+"""
+
+
+import unittest
+from unittest.mock import patch
+from GitHubApi import getResp
+from GitHubApi import getCommits_url
+
+
+class TesttestApi(unittest.TestCase):
+    @patch('testApi.requests.get')
+   
+    def testNumberofRepo(self,mock):
+        mock.get('https://api.github.com/users/sbahala/repos', text='success')
+        self.assertEqual(getResp('https://api.github.com/users/sbahala/repos'), 'success')
+       
+
+    def testCommit(self):
+        commits = getCommits_url('sbahala','Triangle567_2A')
+        self.assertNotEquals(commits,4)
+       
+
+if __name__ == '__main__':
+   unittest.main()
+   
