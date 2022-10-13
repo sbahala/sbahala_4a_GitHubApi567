8001e392d4feaef8ec487f075dc7f2cad9def40a
sbahala <sbahala@stevens.edu>
2022-10-13 06:58:15 +0530

    TestGitHubApi.py

--- TestGitHubApi.py
+++ TestGitHubApi.py
@@ -6,23 +6,17 @@ Test class to test GitHub Api call
 
 
 import unittest
-from unittest.mock import patch
-from GitHubApi import getResp
-from GitHubApi import getCommits_url
+from nose.tools import assert_true
+import requests
 
 
 class TesttestApi(unittest.TestCase):
-    @patch('GitHubApi.requests.get')
-   
-    def testNumberofRepo(self,mock):
-        mock.get('https://api.github.com/users/sbahala/repos', text='success')
-        self.assertEqual(getResp('https://api.github.com/users/sbahala/repos'), 'success')
-       
-
-    def testCommit(self):
-        commits = getCommits_url('sbahala','Triangle567_2A')
-        self.assertNotEquals(commits,4)
-       
+     def testUrlRepo(self):
+         response = requests.get('https://api.github.com/users/sbahala/repos')
+         assert_true(response.ok)
+     def testrepoCommits(self):
+         response = requests.get('https://api.github.com/repos/sbahala/Triangle567_2A/commits')
+         assert_true(response.ok)
 
 if __name__ == '__main__':
    unittest.main()
